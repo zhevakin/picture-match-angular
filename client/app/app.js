f549a8be06ebb73e8f74c07290522d64004243f8
Xvakin <ixvakin@gmail.com>
2015-08-12 12:33:34 +0300

    ngTouch Added

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,11 @@
 import './app.css';
 import angular from 'angular';
+import 'angular-touch';
 import {gameModule, settingsModule, cardModule} from './components/components';
 import {gameServiceModule, instagramModule} from './services/services';
 
 angular.module('app', [
+    'ngTouch',
     settingsModule.name,
     gameModule.name,
     cardModule.name,
